891f6e94fc0e600cc7ba61df9c3c6fbad2814b9a
gustaf <gbergmar@kth.se>
2024-08-04 20:54:58 +0200

    cargo fix

--- src/amm/uniswap_v3/mod.rs
+++ src/amm/uniswap_v3/mod.rs
@@ -833,65 +833,65 @@ impl UniswapV3Pool {
         } else {
             U256::one() << 128
         };
-        if (abs_tick & 0x2 != 0) {
+        if abs_tick & 0x2 != 0 {
             ratio = (ratio * U256::from(0xfff97272373d413259a46990580e213a_u128)) >> 128
         };
-        if (abs_tick & 0x4 != 0) {
+        if abs_tick & 0x4 != 0 {
             ratio = (ratio * U256::from(0xfff2e50f5f656932ef12357cf3c7fdcc_u128)) >> 128
         };
-        if (abs_tick & 0x8 != 0) {
+        if abs_tick & 0x8 != 0 {
             ratio = (ratio * U256::from(0xffe5caca7e10e4e61c3624eaa0941cd0_u128)) >> 128
         };
-        if (abs_tick & 0x10 != 0) {
+        if abs_tick & 0x10 != 0 {
             ratio = (ratio * U256::from(0xffcb9843d60f6159c9db58835c926644_u128)) >> 128
         };
-        if (abs_tick & 0x20 != 0) {
+        if abs_tick & 0x20 != 0 {
             ratio = (ratio * U256::from(0xff973b41fa98c081472e6896dfb254c0_u128)) >> 128
         };
-        if (abs_tick & 0x40 != 0) {
+        if abs_tick & 0x40 != 0 {
             ratio = (ratio * U256::from(0xff2ea16466c96a3843ec78b326b52861_u128)) >> 128
         };
-        if (abs_tick & 0x80 != 0) {
+        if abs_tick & 0x80 != 0 {
             ratio = (ratio * U256::from(0xfe5dee046a99a2a811c461f1969c3053_u128)) >> 128
         };
-        if (abs_tick & 0x100 != 0) {
+        if abs_tick & 0x100 != 0 {
             ratio = (ratio * U256::from(0xfcbe86c7900a88aedcffc83b479aa3a4_u128)) >> 128
         };
-        if (abs_tick & 0x200 != 0) {
+        if abs_tick & 0x200 != 0 {
             ratio = (ratio * U256::from(0xf987a7253ac413176f2b074cf7815e54_u128)) >> 128
         };
-        if (abs_tick & 0x400 != 0) {
+        if abs_tick & 0x400 != 0 {
             ratio = (ratio * U256::from(0xf3392b0822b70005940c7a398e4b70f3_u128)) >> 128
         };
-        if (abs_tick & 0x800 != 0) {
+        if abs_tick & 0x800 != 0 {
             ratio = (ratio * U256::from(0xe7159475a2c29b7443b29c7fa6e889d9_u128)) >> 128
         };
-        if (abs_tick & 0x1000 != 0) {
+        if abs_tick & 0x1000 != 0 {
             ratio = (ratio * U256::from(0xd097f3bdfd2022b8845ad8f792aa5825_u128)) >> 128
         };
-        if (abs_tick & 0x2000 != 0) {
+        if abs_tick & 0x2000 != 0 {
             ratio = (ratio * U256::from(0xa9f746462d870fdf8a65dc1f90e061e5_u128)) >> 128
         };
-        if (abs_tick & 0x4000 != 0) {
+        if abs_tick & 0x4000 != 0 {
             ratio = (ratio * U256::from(0x70d869a156d2a1b890bb3df62baf32f7_u128)) >> 128
         };
-        if (abs_tick & 0x8000 != 0) {
+        if abs_tick & 0x8000 != 0 {
             ratio = (ratio * U256::from(0x31be135f97d08fd981231505542fcfa6_u128)) >> 128
         };
-        if (abs_tick & 0x10000 != 0) {
+        if abs_tick & 0x10000 != 0 {
             ratio = (ratio * U256::from(0x9aa508b5b7a84e1c677de54f3e99bc9_u128)) >> 128
         };
-        if (abs_tick & 0x20000 != 0) {
+        if abs_tick & 0x20000 != 0 {
             ratio = (ratio * U256::from(0x5d6af8dedb81196699c329225ee604_u128)) >> 128
         };
-        if (abs_tick & 0x40000 != 0) {
+        if abs_tick & 0x40000 != 0 {
             ratio = (ratio * U256::from(0x2216e584f5fa1ea926041bedfe98_u128)) >> 128
         };
-        if (abs_tick & 0x80000 != 0) {
+        if abs_tick & 0x80000 != 0 {
             ratio = (ratio * U256::from(0x48a170391f7dc42444e8fa2_u128)) >> 128
         };
 
-        if (tick > 0) {
+        if tick > 0 {
             ratio = U256::max_value() / ratio
         };
 
